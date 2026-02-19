7d3d1d4058c66f22b9fcde5f8c5189bc2e0462db
vanin2004 <160011677+vanin2004@users.noreply.github.com>
2026-02-19 13:17:08 +0300

    создание алгоритма изменения проекции растра

---
+++ backend/src/services/tasks/algorithms/raster_transform.py
@@ -0,0 +1,88 @@
+from typing import override
+
+from osgeo import gdal
+from pydantic import Field
+from src.models.schemas import AlgorithmParamsBaseModel
+
+from . import AlgorithmAbstractFactory, BaseAlgorithm
+
+
+class RasterTransformAlgorithmParams(AlgorithmParamsBaseModel):
+    """Pydantic-модель для параметров алгоритма трансформации растровых данных."""
+
+    srs_def: str = Field(
+        ..., description="Целевая система координат (например, 'EPSG:4326')"
+    )
+    s_srs: str | None = Field(
+        default=None,
+        description="Исходная система координат (необязательно, по умолчанию определяется автоматически)",
+    )
+
+
+@AlgorithmAbstractFactory.register_algorithm("RASTER_TRANSFORM")
+class RasterTransformAlgorithm(BaseAlgorithm):
+    """Алгоритм для трансформации растровых данных."""
+
+    def print_metadata(self, dataset: gdal.Dataset):
+        """Выводит базовую метадату растрового изображения."""
+        print("Basic metadata")
+        # print("Bands count: %s" % dataset.RasterCount)
+        # print("Data type: %s" % dataset.GetRasterBand(1).DataType)
+        # print("Rows: %s | Cols: %s" % (dataset.RasterYSize, dataset.RasterXSize))
+        # print("GeoTransform: %s" % str(dataset.GetGeoTransform()))
+        print(
+            "Coordinate reference system: %s"
+            % dataset.GetProjection()[-len('  AUTHORITY["EPSG","4326"]') :]
+        )
+
+    @override
+    def run(self, input_file_bytes: bytes, file_ext: str) -> bytes:
+        """Трансформирует растровые данные.
+
+        Args:
+            input_file_bytes (bytes): Байтовое представление входного файла.
+        Returns:
+            bytes: Байтовое представление выходного файла.
+        """
+
+        s_srs = self._params.s_srs  # type: ignore[attr-defined]
+        srs_def = self._params.srs_def  # type: ignore[attr-defined]
+
+        gdal.FileFromMemBuffer(f"/vsimem/in.{file_ext}", input_file_bytes)
+
+        opts = gdal.WarpOptions(dstSRS=srs_def, srcSRS=s_srs)
+
+        out_ds = gdal.Warp(
+            f"/vsimem/out.{file_ext}", f"/vsimem/in.{file_ext}", options=opts
+        )
+
+        f = gdal.VSIFOpenL(f"/vsimem/out.{file_ext}", "rb")
+        gdal.VSIFSeekL(f, 0, 2)
+        size = gdal.VSIFTellL(f)
+        gdal.VSIFSeekL(f, 0, 0)
+        result_bytes = gdal.VSIFReadL(1, size, f)
+        gdal.VSIFCloseL(f)
+
+        in_ds: gdal.Dataset = gdal.Open(f"/vsimem/in.{file_ext}")
+        self.print_metadata(in_ds)
+        self.print_metadata(out_ds)
+
+        # cleanup
+        gdal.Unlink(f"/vsimem/in.{file_ext}")
+        gdal.Unlink(f"/vsimem/out.{file_ext}")
+        if out_ds is not None:
+            out_ds = None  # type: ignore[assignment]
+        if in_ds is not None:
+            in_ds = None  # type: ignore[assignment]
+
+        return result_bytes
+
+    @override
+    @classmethod
+    def get_pydantic_model(cls) -> type[RasterTransformAlgorithmParams]:
+        """Возвращает Pydantic-модель для валидации параметров алгоритма.
+
+        Returns:
+            BaseModel: Pydantic-модель параметров алгоритма.
+        """
+        return RasterTransformAlgorithmParams
